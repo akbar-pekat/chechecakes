05431b4d70dfcb88958119d7ab11b0a2ecfabe7d
akbar-pekat <akbarpekat4@gmail.com>
2025-05-16 02:08:58 +0800

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,129 @@
+export const products = [
+  {
+    id: 1,
+    name: "Donat Kentang",
+    variants: [
+      {
+        id: 1.1,
+        name: "Donat Meises",
+        description: "Donat klasik dengan taburan coklat meises yang lumer.",
+        price: 5000,
+        stock: 10,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 1.2,
+        name: "Donat Coklat Kacang",
+        description: "Manis dan gurih! Topping coklat plus kacang renyah.",
+        price: 5000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 1.3,
+        name: "Donat Gula Mini",
+        description: "Simpel & manis. Taburan gula halus yang ringan banget.",
+        price: 5000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 1.4,
+        name: "Donat Krim Keju",
+        description: "Donat lembut dengan isian krim keju creamy yang nagih.",
+        price: 5000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Brownies Panggang",
+    variants: [
+      {
+        id: 2.1,
+        name: "Brownies Panggang Coklat Chip",
+        description: "Brownies klasik dengan topping chip coklat yang meleleh.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 2.2,
+        name: "Brownies Panggang Almond",
+        description: "Gurihnya almond panggang nambah tekstur di tiap gigitan.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 2.3,
+        name: "Brownies Panggang Keju",
+        description:
+          "BBrownies panggang dengan taburan keju asin-manis yang pas.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Bolu",
+    variants: [
+      {
+        id: 3.1,
+        name: "Bolu Marmer",
+        description:
+          "Bolu marmer klasik dengan pola cantik & rasa vanila-coklat seimbang.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 3.2,
+        name: "Bolu Pandan",
+        description:
+          "Wangi pandan yang lembut dan legit, cocok buat temen ngopi.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 3.3,
+        name: "Bolu Jadul",
+        description:
+          "Bolu jadul dengan rasa telur manis khas nostalgia rumah nenek.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+      {
+        id: 3.4,
+        name: "Bolu Tape",
+        description:
+          "Lembut dan wangi tape singkong, khas banget dan bikin pengen lagi.",
+        price: 20000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Brownies Kukus",
+    variants: [
+      {
+        id: 4.1,
+        name: "Brownies Kukus",
+        description:
+          "Lembut, moist, dan manisnya pas! Brownies kukus favorit banyak orang 🎂",
+        price: 30000,
+        image:
+          "https://dummyimage.com/600x400/000/fff",
+      },
+    ],
+  },
+];
