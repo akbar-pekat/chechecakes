2abb07603f2d915ee3b1dcca9a7b4f38e88610b0
akbar-pekat <akbarpekat4@gmail.com>
2025-05-27 04:01:34 +0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,24 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   output: "server",
+  site: "https://chechecakes.biz.id",
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        page !== "https://chechecakes.biz.id/checkout/" &&
+        page !== "https://chechecakes.biz.id/payment/cod" &&
+        page !== "https://chechecakes.biz.id/payment/ots" &&
+        page !== "https://chechecakes.biz.id/payment/tf",
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date("2025-05-27"),
+      customPages: ["https://chechecakes.biz.id/blog/*"],
+    }),
+  ],
 });
